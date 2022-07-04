bffcc5dbba573ca767cb8b96f6adf0b649be9b14
moked1 <moked1@tmprotection.co.il>
2022-07-04 21:11:14 +0300

    will be alright

--- logic.js
+++ logic.js
@@ -1,1 +1,21 @@
-a
+const fetch = require('node-fetch');
+
+const encodedParams = new URLSearchParams();
+encodedParams.append("apiKey", "<REQUIRED>");
+
+const url = 'https://strainraygorodskijv1.p.rapidapi.com/getAllStrains';
+
+const options = {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'StrainraygorodskijV1.p.rapidapi.com'
+  },
+  body: encodedParams
+};
+
+fetch(url, options)
+	.then(res => res.json())
+	.then(json => console.log(json))
+	.catch(err => console.error('error:' + err));
